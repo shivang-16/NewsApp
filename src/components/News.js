4913f9852b0f309b21221f330e13829dca2a85f0
Shivang Yadav <shivangyadav121@gmail.com>
2023-07-01 18:43:51 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -42,7 +42,7 @@ export default class News extends Component {
 
     document.title =
       this.props.category === "General"
-        ? document.title
+        ? ' NewsSphere - Latest News, Breaking Stories, and In-Depth Analysis'
         : `NewsSphere - ${this.props.category}`;
   }
 
